d832f96b913a05ce9aeaa355c013039058e00de0
JPO100 <john@johnoliver.me>
2026-05-09 11:37:52 +0100

    tutorials: remove clipped bottom gap from reminder pages

--- src/app/components/OnboardingPage1Content.tsx
+++ src/app/components/OnboardingPage1Content.tsx
@@ -52,7 +52,7 @@ export default function OnboardingPage1Content() {
   }, [animationKey]);
 
   return (
-    <div className="content-stretch flex flex-col flex-1 min-h-0 gap-[22.334px] items-center pb-[28.334px] pt-[10px] px-[14px] relative w-full">
+    <div className="content-stretch flex flex-col flex-1 min-h-0 gap-[22.334px] items-center pt-[10px] px-[14px] relative w-full">
       <TutorialStaticReminderList />
     </div>
   );
--- src/app/components/OnboardingPage6Content.tsx
+++ src/app/components/OnboardingPage6Content.tsx
@@ -134,7 +134,7 @@ export default function OnboardingPage6Content() {
   }, []);
 
   return (
-    <div className="content-stretch flex flex-col flex-1 min-h-0 gap-[22.334px] items-center pb-[28.334px] pt-[10px] px-[14px] relative w-full">
+    <div className="content-stretch flex flex-col flex-1 min-h-0 gap-[22.334px] items-center pt-[10px] px-[14px] relative w-full">
       <TutorialStaticReminderList />
     </div>
   );
