fc6a45471743c1e06f8314f5073d4fffb8036a84
JPO100 <john@johnoliver.me>
2026-05-09 11:15:24 +0100

    tutorials: fix subtitle blocks to 60px height

--- src/app/components/tutorialTokens.ts
+++ src/app/components/tutorialTokens.ts
@@ -2,4 +2,4 @@ export const TUTORIAL_TITLE_CLASSNAME =
     "css-g0mm18 flex flex-col font-['Lato:Bold',sans-serif] justify-center leading-[0] not-italic relative shrink-0 text-[#1c2c42] text-[22px] text-center";
 
 export const TUTORIAL_BODY_CLASSNAME =
-    "flex flex-col font-['Lato:Medium',sans-serif] justify-center leading-[0] not-italic relative shrink-0 text-[#939393] text-[17px] text-center w-full max-w-[322px]";
+    "flex h-[60px] flex-col justify-start font-['Lato:Medium',sans-serif] leading-[0] not-italic relative shrink-0 text-[#939393] text-[17px] text-center w-full max-w-[322px]";
